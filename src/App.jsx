2cf9ed7d08c0db8b59d5789213354452fa4174e1
laurabrilhantee <laurabrilhante8@gmail.com>
2026-04-15 16:02:02 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import logo from './assets/image.png'
 
 export default function App() {
 
 return (
   <main>
     <section>
+      
       <h1>Facebook</h1>
 
       <input type="text" placeholder='Email ou telefone'/>
