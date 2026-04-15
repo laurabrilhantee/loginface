df5d88ceb6656116f2e8c0b910536126f21c3c02
laurabrilhantee <laurabrilhante8@gmail.com>
2026-04-15 15:31:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+return (
+  <main>
+    <section>
+      <h1>Facebook</h1>
+
+      <input type="text" placeholder='Email ou telefone'/>
+      <input type="password" placeholder='Senha' />
+
+      <button>Entrar</button>
+
+      <div className="forgot">Esqueceu a senha?</div>
+
+      <hr className="linha" />
+
+       <button className="segundo">Criar nova conta</button>
+
+
+      
+    </section>
+  </main>
+)  
+
+}
