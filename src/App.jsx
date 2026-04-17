02f40b8c20aeabe26b73c4b63ea0d3d6a5323f9b
laurabrilhantee <laurabrilhante8@gmail.com>
2026-04-17 10:34:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ export default function App() {
 
 return (
   <main>
+
+    <div className="left">
+        <img src={logo} className="logo" />
+       </div>
+       
     <section>
       
       <h1>Facebook</h1>
+
 
       <input type="text" placeholder='Email ou telefone'/>
       <input type="password" placeholder='Senha' />
